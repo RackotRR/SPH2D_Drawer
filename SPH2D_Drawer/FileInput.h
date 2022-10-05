cc863937818737f3d75e5b502d2eeac72785828e
AsrankulovAleksandr <Asrankulov@benice.games>
2022-10-05 22:20:18 +0300

    update file input

---
+++ SPH2D_Drawer/FileInput.h
@@ -0,0 +1,94 @@
+#pragma once
+#include <vector> 
+#include <fstream>
+#include <string>
+#include <thread>
+#include <filesystem>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+#include "RRGrapher.h"
+
+
+struct Params {
+	std::string experimentName;
+	double x_origin;
+	double y_origin;
+	double x_size;
+	double y_size;
+	double dx;
+	double dy;
+	double dt;
+	double simulationTime;
+	int save_step;
+};
+
+Params loadParams(std::string filePath) {
+	nlohmann::json json;
+	std::ifstream stream{ filePath };
+	stream >> json;
+
+	Params params;
+	json.at("x_mingeom").get_to(params.x_origin);
+	json.at("y_mingeom").get_to(params.y_origin);
+	json.at("x_size").get_to(params.x_size);
+	json.at("y_size").get_to(params.y_size);
+	json.at("dx").get_to(params.dx);
+	json.at("dy").get_to(params.dy);
+	json.at("dt").get_to(params.dt);
+	json.at("simulation_time").get_to(params.simulationTime);
+	json.at("save_step").get_to(params.save_step);
+	json.at("experiment_name").get_to(params.experimentName);
+	return params;
+}
+
+void loadLayerFromFile(std::string path, TimeLayer& layer) {
+	int layerCounter{};
+	std::ifstream stream(path);
+
+	size_t ntotal;
+
+	while (stream >> ntotal) {
+		for (size_t i{}; i < ntotal; i++) {
+			double x, y;
+			int type;
+			stream >> x >> y;
+			stream >> type;
+			layer.emplace(x, y, type);
+		}
+	}
+
+	std::filesystem::path p = path;
+	std::string output = "layer " + p.filename().string() + "\n";
+	std::cout << output;
+}
+
+auto ReadGridAndParams(std::string dirPath) {
+	std::tuple<Grid, Square, double> tuple;
+	auto& [grid, square, dx] = tuple;
+	auto& [origin, size] = square;
+
+	Params params = loadParams(dirPath + "\\Params.json");
+	dx = params.dx;
+	origin.first = params.x_origin;
+	origin.second = params.y_origin;
+	size.first = params.x_size;
+	size.second = params.y_size;
+
+	std::vector<std::jthread> threads;
+
+	int step = 0;
+	while (true) {
+		auto path = std::filesystem::current_path().append(dirPath + "\\" + std::to_string(step));
+		if (std::filesystem::exists(path)) {
+			grid.emplace_back();
+			threads.emplace_back(loadLayerFromFile, path.string(), std::ref(grid.back()));
+			step += params.save_step;
+		}
+		else {
+			break;
+		}
+	}
+
+	return tuple;
+}
--- SPH2D_Drawer/Source.cpp
+++ SPH2D_Drawer/Source.cpp
@@ -1,53 +1,12 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector> 
-#include <functional>
-#include <optional>
-#include <cmath>
 
 #include "RRGrapher.h"  
+#include "FileInput.h"
 
-auto ReadGridAndParams(std::string_view filePath) {
-	std::tuple<Grid, Square, double> tuple;
-	auto& [grid, square, dx] = tuple;
-	auto& [origin, size] = square;
-
-	double dy;
-	size_t ntotal;
-
-	std::ifstream stream{ filePath.data() };
-	std::cout << "open " << filePath << std::endl;
-
-	stream >> origin.first;
-	stream >> origin.second;
-	stream >> size.first;
-	stream >> size.second;
-	stream >> dx;
-	stream >> dy;
-
-	std::cout << "read particles" << std::endl;
-	int layerCounter{};
-
-	while (stream >> ntotal) {
-		TimeLayer layer;
-		std::cout << "layer " << layerCounter++ << std::endl;
-		for (size_t i{}; i < ntotal; i++) {
-			double x, y;
-			int type;
-			stream >> x >> y;
-			stream >> type;
-			layer.emplace(x, y, type);
-		}
-		grid.push_back(std::move(layer));
-	}
-
-	return tuple;
-}
 
 int main(int argc, char* argv[]) {      
 	std::string str;
-	std::cout << "params file: " << std::endl;
+	std::cout << "experiment directory: " << std::endl;
 	std::cin >> str;
 
 	try {
